bdc448e1fb0335122f8d5c152bca4fc7f781883c
kelvinbiffi <kelvin.biffi@hotmail.com>
2017-06-27 11:50:07 -0300

    Added shape to button play at splash activity, added music to splash screen

--- app/src/main/java/kelvinbiffi/com/taptowin/Splash.java
+++ app/src/main/java/kelvinbiffi/com/taptowin/Splash.java
@@ -1,13 +1,48 @@
 package kelvinbiffi.com.taptowin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
+
+    Button btnPlay;
+    MediaPlayer ninja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        configButton();
+
+        playSong();
+    }
+
+    private void configButton(){
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/orchidee_medium.ttf");
+        btnPlay.setTypeface(font);
+    }
+
+    private void playSong(){
+        try {
+            ninja = new MediaPlayer();
+
+            AssetFileDescriptor descriptor = getAssets().openFd("sounds/ninja_star_inn.mp3");
+            ninja.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+            descriptor.close();
+
+            ninja.prepare();
+            ninja.setVolume(1f,1f);
+            ninja.setLooping(true);
+            ninja.start();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
